8bdb7fe6a8ac12dd24f9b960ece9178ca60cdc58
lmd278 <palered548@gmail.com>
2022-10-06 12:23:48 +0700

    Fix deprecated

--- .config/nvim/lua/user/bufferline2.lua
+++ .config/nvim/lua/user/bufferline2.lua
@@ -5,7 +5,7 @@ end
 
 bufferline.setup {
     options = {
-        close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
+        close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
         right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
         offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
         separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' },
--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -55,33 +55,32 @@ end
 local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true }
     local keymap = vim.api.nvim_buf_set_keymap
-    keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
-    keymap(bufnr, "v", "<leader>lf", "<cmd>lua vim.lsp.buf.range_formatting()<cr>", opts)
-    keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-    keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
-    keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
-    keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
-    keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-    keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    keymap(bufnr, "n", "K", "<CMD>lua vim.lsp.buf.hover()<CR>", opts)
+    keymap(bufnr, "n", "gd", "<CMD>lua vim.lsp.buf.definition()<CR>", opts)
+    keymap(bufnr, "n", "gD", "<CMD>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap(bufnr, "n", "gI", "<CMD>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap(bufnr, "n", "gl", "<CMD>lua vim.diagnostic.open_float()<CR>", opts)
+    keymap(bufnr, "n", "gr", "<CMD>lua vim.lsp.buf.references()<CR>", opts)
+    keymap(bufnr, "n", "<leader>la", "<CMD>lua vim.lsp.buf.code_action()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lf", "<CMD>lua vim.lsp.buf.format { async = true }<CR>", opts)
+    keymap(bufnr, "n", "<leader>li", "<CMD>LspInfo<CR>", opts)
+    keymap(bufnr, "n", "<leader>lI", "<CMD>LspInstallInfo<CR>", opts)
+    keymap(bufnr, "n", "<leader>lj", "<CMD>lua vim.diagnostic.goto_next { buffer = 0 }<CR>", opts)
+    keymap(bufnr, "n", "<leader>lk", "<CMD>lua vim.diagnostic.goto_prev { buffer = 0 }<CR>", opts)
+    keymap(bufnr, "n", "<leader>lq", "<CMD>lua vim.diagnostic.setloclist()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lr", "<CMD>lua vim.lsp.buf.rename()<CR>", opts)
+    keymap(bufnr, "n", "<leader>ls", "<CMD>lua vim.lsp.buf.signature_help()<CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
     if client.name == "tsserver" then
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
     end
 
     if client.name == "sumneko_lua" then
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
     end
 
     lsp_keymaps(bufnr)
