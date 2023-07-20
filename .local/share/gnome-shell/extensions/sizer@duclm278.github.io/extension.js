2f0665459765c5e7808f0ffa014f6d4dbd81de64
duclm278 <palered548@gmail.com>
2023-07-20 11:29:50 +0700

    Add more methods for sizer

--- .local/share/gnome-shell/extensions/sizer@duclm278.github.io/extension.js
+++ .local/share/gnome-shell/extensions/sizer@duclm278.github.io/extension.js
@@ -1,8 +1,4 @@
 /**
- * By orzun
- * Modified by duclm278
- * License: GPLv3
- *
  * Install:
  * 1. Run: make install
  * 2. X11: Restart gnome-shell (Alt+F2, r, Enter)
@@ -10,7 +6,15 @@
  * 4. Run: gnome-extensions enable sizer@duclm278.github.io
  *
  * Usage:
- * gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Sizer --method org.gnome.Shell.Extensions.Sizer.MoveResize 0 0 1600 900
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.Get
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.Move 0 0
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.MoveResize 0 0 1600 900
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.Resize 1600 900
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.MoveInMonitor 0 0
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.MoveResizeInMonitor 0 0 1600 900
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.MoveInWorkArea 0 0
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.MoveResizeInWorkArea 0 0 1600 900
+ * gdbus call --session --dest org.gnome.Shell --object-path /dev/duc/Sizer --method dev.duc.Sizer.CenterInWorkArea
  */
 
 const { Gio, GLib, Clutter, Meta, Shell, St } = imports.gi;
@@ -21,7 +25,7 @@ const MESSAGE_FADE_TIME = 2000;
 
 const MR_DBUS_IFACE = `
 <node>
-    <interface name="org.gnome.Shell.Extensions.Sizer">
+    <interface name="dev.duc.Sizer">
         <method name="Get">
         </method>
         <method name="Move">
@@ -34,19 +38,63 @@ const MR_DBUS_IFACE = `
             <arg type="u" direction="in" name="width"/>
             <arg type="u" direction="in" name="height"/>
         </method>
+        <method name="Resize">
+            <arg type="u" direction="in" name="width"/>
+            <arg type="u" direction="in" name="height"/>
+        </method>
+        <method name="MoveInMonitor">
+            <arg type="u" direction="in" name="x"/>
+            <arg type="u" direction="in" name="y"/>
+        </method>
+        <method name="MoveResizeInMonitor">
+            <arg type="u" direction="in" name="x"/>
+            <arg type="u" direction="in" name="y"/>
+            <arg type="u" direction="in" name="width"/>
+            <arg type="u" direction="in" name="height"/>
+        </method>
+        <method name="MoveInWorkArea">
+            <arg type="u" direction="in" name="x"/>
+            <arg type="u" direction="in" name="y"/>
+        </method>
+        <method name="MoveResizeInWorkArea">
+            <arg type="u" direction="in" name="x"/>
+            <arg type="u" direction="in" name="y"/>
+            <arg type="u" direction="in" name="width"/>
+            <arg type="u" direction="in" name="height"/>
+        </method>
+        <method name="CenterInWorkArea">
+        </method>
     </interface>
 </node>`;
 
 class Extension {
   enable() {
     this._dbus = Gio.DBusExportedObject.wrapJSObject(MR_DBUS_IFACE, this);
-    this._dbus.export(Gio.DBus.session, "/org/gnome/Shell/Extensions/Sizer");
+    this._dbus.export(Gio.DBus.session, "/dev/duc/Sizer");
   }
 
   disable() {
     this._dbus.flush();
     this._dbus.unexport();
     delete this._dbus;
+  }
+
+  _getWindow() {
+    return global.display.get_focus_window();
+  }
+
+  _getMonitorIndex() {
+    return global.display.get_current_monitor();
+  }
+
+  _getMonitorGeometry() {
+    return global.display.get_monitor_geometry(this._getMonitorIndex());
+  }
+
+  _getWorkArea() {
+    return global.workspace_manager
+      .get_active_workspace()
+      .get_work_area_for_monitor(this._getMonitorIndex());
   }
 
   _flashMessage(message) {
@@ -60,7 +108,8 @@ class Extension {
 
     this._text.opacity = 255;
 
-    const monitor = Main.layoutManager.primaryMonitor;
+    const monitor = this._getMonitorGeometry();
+    // const monitor = Main.layoutManager.primaryMonitor;
     this._text.set_position(
       monitor.x + Math.floor(monitor.width / 2 - this._text.width / 2),
       monitor.y + Math.floor(monitor.height / 2 - this._text.height / 2)
@@ -81,65 +130,176 @@ class Extension {
 
   _notifySizeChange(window) {
     const { scaleFactor } = St.ThemeContext.get_for_stage(global.stage);
-    let newOuterRect = window.get_frame_rect();
-    let newInnerRect = window.get_buffer_rect();
+    let newFrameRect = window.get_frame_rect();
     let message = "";
-    message += "Pos: (%d,%d)\n".format(
-      newOuterRect.x / scaleFactor,
-      newOuterRect.y / scaleFactor
+    message += "Pos: (%d, %d)\n".format(
+      newFrameRect.x / scaleFactor,
+      newFrameRect.y / scaleFactor
     );
-    message += "Inner: %dx%d\n".format(
-      newInnerRect.width / scaleFactor,
-      newInnerRect.height / scaleFactor
+    message += "Size: %dx%d\n".format(
+      newFrameRect.width / scaleFactor,
+      newFrameRect.height / scaleFactor
     );
-    message += "Outer: %dx%d".format(
-      newOuterRect.width / scaleFactor,
-      newOuterRect.height / scaleFactor
+
+    const monitor = this._getMonitorGeometry();
+    message += "Monitor: %dx%d\n".format(
+      monitor.width / scaleFactor,
+      monitor.height / scaleFactor
+    );
+
+    const workArea = this._getWorkArea();
+    message += "WorkArea: %dx%d".format(
+      workArea.width / scaleFactor,
+      workArea.height / scaleFactor
     );
 
     this._flashMessage(message);
   }
 
+  _isWithinWorkArea(area, workArea) {
+    return (
+      area.x >= workArea.x &&
+      area.y >= workArea.y &&
+      area.x + area.width <= workArea.x + workArea.width &&
+      area.y + area.height <= workArea.y + workArea.height
+    );
+  }
+
+  _isEntireWorkAreaWidth(area) {
+    const workArea = this._getWorkArea();
+    if (
+      this._isWithinWorkArea(area, workArea) &&
+      area.x === workArea.x &&
+      area.width === workArea.width
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  _isEntireWorkAreaHeight(area) {
+    const workArea = this._getWorkArea();
+    if (
+      this._isWithinWorkArea(area, workArea) &&
+      area.y === workArea.y &&
+      area.height === workArea.height
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   Get() {
-    const window = global.display.get_focus_window();
+    const window = this._getWindow();
     if (!window) return;
 
     this._notifySizeChange(window);
   }
 
   Move(x, y) {
-    const window = global.display.get_focus_window();
+    const window = this._getWindow();
     if (!window) return;
 
-    if (window.get_maximized() !== 0) {
+    if (window.get_maximized()) {
       window.unmaximize(Meta.MaximizeFlags.BOTH);
     }
 
-    const posId = window.connect("position-changed", () => {
-      window.disconnect(posId);
-      this._notifySizeChange(window);
-    });
-    window.move_frame(1, x, y);
+    window.move_frame(true, x, y);
   }
 
   MoveResize(x, y, width, height) {
-    const window = global.display.get_focus_window();
+    const area = { x, y, width, height };
+    const window = this._getWindow();
     if (!window) return;
 
-    if (window.get_maximized() !== 0) {
+    // GNOME has its own built-in tiling that is activated when pressing
+    // Super+Left/Right. There does not appear to be any way to detect this
+    // through the Meta APIs, so we always unmaximize to break the tiling.
+    if (window.get_maximized()) {
       window.unmaximize(Meta.MaximizeFlags.BOTH);
     }
 
-    const posId = window.connect("position-changed", () => {
-      window.disconnect(posId);
-      this._notifySizeChange(window);
+    window.move_resize_frame(true, x, y, width, height);
+
+    if (this._isEntireWorkAreaWidth(area)) {
+      window.maximize(Meta.MaximizeFlags.HORIZONTAL);
+    } else {
+      window.unmaximize(Meta.MaximizeFlags.HORIZONTAL);
+    }
+
+    if (this._isEntireWorkAreaHeight(area)) {
+      window.maximize(Meta.MaximizeFlags.VERTICAL);
+    } else {
+      window.unmaximize(Meta.MaximizeFlags.VERTICAL);
+    }
+
+    // In some cases move_resize_frame() will only resize the window, and we
+    // must call move_frame() to move it. This usually happens when the
+    // window's minimum size is larger than the selected area. Movement can
+    // also be a bit glitchy on Wayland. We therefore make extra attempts,
+    // alternating between move_frame() and move_resize_frame().
+    let attempts = 0;
+    GLib.timeout_add(GLib.PRIORITY_DEFAULT, 20, () => {
+      const frame = window.get_frame_rect();
+      if (
+        frame.x === x &&
+        frame.y === y &&
+        frame.width === width &&
+        frame.height === height
+      ) {
+        return GLib.SOURCE_REMOVE;
+      }
+
+      if (attempts % 2 === 0) {
+        window.move_frame(true, x, y);
+      } else {
+        window.move_resize_frame(true, x, y, width, height);
+      }
+
+      if (attempts++ >= 5) {
+        return GLib.SOURCE_REMOVE;
+      }
+
+      return GLib.SOURCE_CONTINUE;
     });
-    const sizeId = window.connect("size-changed", () => {
-      window.disconnect(sizeId);
-      this._notifySizeChange(window);
-    });
-    window.move_frame(1, x, y);
-    window.move_resize_frame(1, x, y, width, height);
+  }
+
+  Resize(width, height) {
+    const window = this._getWindow();
+    const frame = window.get_frame_rect();
+    this.MoveResize(frame.x, frame.y, width, height);
+  }
+
+  MoveInMonitor(x, y) {
+    const monitor = this._getMonitorGeometry();
+    this.Move(monitor.x + x, monitor.y + y);
+  }
+
+  MoveResizeInMonitor(x, y, width, height) {
+    const monitor = this._getMonitorGeometry();
+    this.MoveResize(monitor.x + x, monitor.y + y, width, height);
+  }
+
+  MoveInWorkArea(x, y) {
+    const workArea = this._getWorkArea();
+    this.Move(workArea.x + x, workArea.y + y);
+  }
+
+  MoveResizeInWorkArea(x, y, width, height) {
+    const workArea = this._getWorkArea();
+    this.MoveResize(workArea.x + x, workArea.y + y, width, height);
+  }
+
+  CenterInWorkArea() {
+    const window = this._getWindow();
+    const workArea = this._getWorkArea();
+    if (!window) return;
+
+    const frame = window.get_frame_rect();
+    const x = workArea.x + Math.floor((workArea.width - frame.width) / 2);
+    const y = workArea.y + Math.floor((workArea.height - frame.height) / 2);
+
+    this.Move(x, y);
   }
 }
 
